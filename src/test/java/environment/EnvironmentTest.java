2abd0f4342e43b81d3fd3d52f30e4fb9eb36f654
ongky-febriandy <febriandy.ongky@gmail.com>
2022-03-14 16:55:19 +0700

    Environment

---
+++ src/test/java/environment/EnvironmentTest.java
@@ -0,0 +1,27 @@
+package environment;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest(classes = EnvironmentTest.TestApplication.class)
+public class EnvironmentTest {
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void testEnvironment() {
+        String javaHome = environment.getProperty("JAVA_HOME");
+//        Assertions.assertEquals();
+    }
+
+    @SpringBootApplication
+    public static class TestApplication{
+
+    }
+}
