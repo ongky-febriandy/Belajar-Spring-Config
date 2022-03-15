02cf620bacf43a5b6d52661952abb6009af92d87
Calvin <febriandy.ongky@gmail.com>
2022-03-15 11:14:24 +0700

    Test Property Source

---
+++ src/test/java/com/programmerzamannow/spring/config/testtpropertysource/PropertySourceTest2.java
@@ -0,0 +1,50 @@
+package com.programmerzamannow.spring.config.testtpropertysource;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
+
+@TestPropertySources({
+        @TestPropertySource("classpath:/test.properties")
+})
+
+@SpringBootTest(classes = PropertySourceTest2.TestApplication.class)
+
+public class PropertySourceTest2 {
+
+
+    @Autowired
+    private TestApplication.SampleProperties properties;
+
+    @Test
+    void testPropertySource() {
+        Assertions.assertEquals("Sample Project Test", properties.getName());
+        Assertions.assertEquals(1, properties.getVersion());
+
+    }
+
+
+    public static class TestApplication{
+
+        @Component
+        @Getter
+        public static class SampleProperties{
+
+            @Value("${sample.name}")
+            private String name;
+
+            @Value("${sample.version}")
+            private Integer version;
+        }
+
+    }
+}
